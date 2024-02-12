a6f5ff3675763308708b9a34d1f6104536dc695c
MrTofuuu <chris.stallcup@gmail.com>
2024-02-12 15:35:22 -0600

    concurrently now working with typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,26 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from "postgres";
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+import express, { Express, Request, Response } from 'express';
 
-const connectionString = "..."
-const sql = postgres(connectionString, { max: 1 })
-const db = drizzle(sql);
+const app: Express = express();
+const PORT = process.env.PORT || 3001;
 
-await migrate(db, { migrationsFolder: "drizzle" });
+// const connectionString = "..."
+// const sql = postgres(connectionString, { max: 1 })
+// const db = drizzle(sql);
 
-await sql.end();
+// await migrate(db, { migrationsFolder: "drizzle" });
+
+// await sql.end();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Express + TypeScript Server');
+});
+
+app.listen(PORT, () =>
+	console.log(`Backend now listening \nhttp://localhost:${PORT}`)
+);
